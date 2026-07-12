72a2818234ce7809ce0065b62db0a1a18fbf5cfa
iwan-semjonow-dev <iwan.semjonow.dev@gmail.com>
2026-07-12 18:12:28 +0200

    add homework 03

---
+++ homework_03/express-ts-server-03/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT} http://localhost:${3000}`);
+})
